311632c5cecaeb083c0ec789e1f35dfd3e7d1af2
Oleksandr Mochalov <molych69@gmail.com>
2020-12-03 20:14:08 +0200

    Added list rendering

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
             inventory: 100,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ]
         }
     }
 })
