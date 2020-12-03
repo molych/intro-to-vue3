61f884e5dcb31ad51e496f65182e661a045c0153
Oleksandr Mochalov <molych69@gmail.com>
2020-12-04 00:03:47 +0200

    Added form & review list

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -37,8 +37,18 @@ app.component('product-display', {
             v-on:click="addToCart">
             Add to Cart
           </button>
+
+          <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          @click="removeFromCart">
+          Remove Item
+        </button>
         </div>
       </div>
+      <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
       return {
@@ -46,6 +56,7 @@ app.component('product-display', {
           brand: 'Vue Mastery',
           selectedVariant: 0,
           details: ['50% cotton', '30% wool', '20% polyester'],
+          reviews: [],
           variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
@@ -54,10 +65,16 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
+        removeFromCart() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+          },
         updateVariant(index) {
             this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
     },
     computed: {
